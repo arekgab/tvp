70edaf4594d4abcbc2566b63f76e46fd898f8ce8
Arek <arkadiusz.gabiga@gmail.com>
2013-05-28 23:14:02 +0200

    Clean event approach

--- app/js/kabarety.js
+++ app/js/kabarety.js
@@ -3,196 +3,106 @@ var HTTP = require('http');
 var URL = require('url');
 var CHEERIO = require('cheerio');
 var REQUEST = require('request');
-var FUTURES = require('futures');
-var ASYNC = require('async');
+var Events = require('events');
 
+var EventType = {
+    BeginProcess: "beginEvent",
+    IdvEvent: "idvEvent",
+    PlayerObjectId: "playerEvent",
+    NumberObjectId: "numberEvent",
+    SpawnVlc: "SpawnVlcEvent",
+    EndRequest: "endEvent"
 
+}
 
-var videoGrabber = function() {
+function VideoGrabber() {
+    
+    var res = null;
+    var eventBus = new Events.EventEmitter();
+
     var CONSTANTS = {
         tvpVideoInfo: "http://www.tvp.pl/pub/stat/videofileinfo?video_id=",
         kabaretLoadSkecz: "http://kabaret.tworzymyhistorie.pl/inc/load_skecz.php?i=",
         initParams: "object param[name='InitParams']",
         kabaretIframe: "iframe[src*='tvp.pl']",
         kabaretMainIDVparam: "input[name='idv']",
-        stateOK : "OK",
-        stateERROR : "ERROR"
+        stateOK: "OK",
+        stateERROR: "ERROR"
     };
-    
+
     var state = CONSTANTS.stateOK;
-    
-    this.getState = function() {
+
+    function getState() {
         return state;
     }
 
-    this.getAddressParam = function(request) {
+    this.getAddressParam = function (request) {
         var urlInfo = URL.parse(request.url, true);
         return urlInfo.query.address;
-    };
+    }
+    ;
 
-    function spawnVLC(videoUrl) {
-        PROCESS.spawn("vlc", [videoUrl], {detached: true});
-    };
-
-    function parseAndLaunchTvpVideoInfo(objectId) {
-        var url = CONSTANTS.tvpVideoInfo + objectId
-
-        REQUEST(url, function(err, resp, body) {
-            console.log("Getting: " + url);
-
-            var json = JSON.parse(body);
-//        console.log("BODY: " + body)
-            console.log("VideoUrl: " + json.video_url);
-            spawnVLC(json.video_url);
-        });
-    };
-
-    function getTvpObjectIdFromURL(tvpUrl) {
-        return tvpUrl.substring(tvpUrl.lastIndexOf('/') + 1);
-    };
-
-    function getTvpObjectIdFromBody(iframeUrl) {
-        console.log("Looking for objectId in body...");
-        REQUEST(iframeUrl, function(err, resp, body) {
-            console.log("Getting iframeUrl: " + iframeUrl);
-            $ = CHEERIO.load(body);
-            var tvpUrl = $(CONSTANTS.initParams).attr("value");
-            var arr = tvpUrl.split(",");
-            var objectId = "xxxxx";
-            for (var i = 0; i < arr.length; i++) {
-                if (arr[i].indexOf("video_id") == 0) {
-                    objectId = arr[i].split("=")[1];
-                    break;
-                }
-            }
-            console.log("video_id param: " + objectId);
-            parseAndLaunchTvpVideoInfo(objectId);
-        });
-    };
-
-    function getPopupPageContents(idv) {
-        var url = CONSTANTS.kabaretLoadSkecz + idv;
-        REQUEST(url, function(err, resp, body) {
-            console.log("Getting: " + url);
-            $ = CHEERIO.load(body);
-            var tvpUrl = $(CONSTANTS.kabaretIframe).attr('src');
-            console.log("tvpUrl: " + tvpUrl);
-            var objectId = getTvpObjectIdFromURL(tvpUrl);
-            console.log("objectId: " + objectId);
-            if (objectId != 'player') {
-                parseAndLaunchTvpVideoInfo(objectId);
-            } else {
-                getTvpObjectIdFromBody(tvpUrl);
-            }
-        });
-    };
-    
-    this.getMainPageContents = function(url) {
-//        console.log("URL from param: " + url);
-//        REQUEST(url, function(err, resp, body) {
-//            if (resp.statusCode == 200) {
-//                $ = CHEERIO.load(body);
-//                var idv = $(CONSTANTS.kabaretMainIDVparam).val();
-//                console.log("idv: " + idv);
-//
-//                getPopupPageContents(idv);
-//            }
-//        });  
-
-//        test(url);
-        test2(url);
-        return state;
-    };
-    
-    function test2(url){
-        ASYNC.waterfall([
-            function(callback) {
-                getIdvParamFormAddress(url, callback);
-            },  
-            function(idv, callback) {
-                getObjectIdFromPageSkecz(idv, callback);
-            },        
-            function(objectId, tvpUrl, callback) {
-                processObjectId(objectId, tvpUrl, callback);
-            },         
-            function(frameUrl, callback) {
-                processFrameContent(frameUrl, callback);
-            }                                
-        ], function(err, result){
-            if(err != null) {
-                console.log("FATAL ERROR: " + JSON.stringify(err));
-            } else {
-                console.log("OKIDOKI");
-            }
-        });
-        
-        
-        console.log("dupa");
-    };
-    
-    function getIdvParamFormAddress(url, next) {
+    function getIdvParamFormAddress(url) {
         console.log("RUN request");
-        REQUEST(url, function(err, response, body){
+        REQUEST(url, function(err, response, body) {
             $ = CHEERIO.load(body);
             var idv = $(CONSTANTS.kabaretMainIDVparam).val();
             console.log("idv: " + idv);
-            next(null, idv);
+
+            eventBus.emit(EventType.IdvEvent, idv);
         });
-    };
-    
-    function getObjectIdFromPageSkecz(idv, next){
+    }
+    ;
+
+    function getTvpObjectIdFromURL(tvpUrl) {
+        return tvpUrl.substring(tvpUrl.lastIndexOf('/') + 1);
+    }
+    ;
+
+    function getObjectIdFromPageSkecz(idv) {
         var url = CONSTANTS.kabaretLoadSkecz + idv;
         console.log("Getting: " + url);
-        REQUEST(url,function(err, response, body){ 
+        REQUEST(url, function(err, response, body) {
             $ = CHEERIO.load(body);
             var tvpUrl = $(CONSTANTS.kabaretIframe).attr('src');
             console.log("tvpUrl: " + tvpUrl);
             var objectId = getTvpObjectIdFromURL(tvpUrl);
             console.log("objectId: " + objectId);
 
-            next(null, objectId, tvpUrl);
+            if (objectId == "player") {
+                eventBus.emit(EventType.PlayerObjectId, tvpUrl);
+            } else {
+                eventBus.emit(EventType.NumberObjectId, objectId);
+            }
         });
-    };
-    
-    function processTvpVideoInfo(objectId, next){
+    }
+    ;
+
+
+    function processTvpVideoInfo(objectId) {
         var urlInfo = CONSTANTS.tvpVideoInfo + objectId;
-        REQUEST(urlInfo, function(err, response, body){
+        REQUEST(urlInfo, function(err, response, body) {
             console.log("Getting: " + urlInfo);
             var json = JSON.parse(body);
             console.log("VideoUrl: " + json.video_url);
-            spawnVLC(json.video_url); 
-            next(err);
+
+            eventBus.emit(EventType.SpawnVlc, json.video_url);
         });
     }
-         
-    function runVlc(objectId, next) {
-        ASYNC.waterfall([
-            function(callback){
-                processTvpVideoInfo(objectId, callback);
-            }
-        ], function(err, result){
-            if(err != null) {
-                console.log("FATAL ERROR: " + JSON.stringify(err));
-            }
-            next(err);
-        });   
-    } 
-     
-    function processObjectId(objectId, tvpUrl, next){
-        console.log("Trying to launch player..");
-        if (objectId != 'player') {
-            console.log("Trigger vlc...")
-            runVlc(objectId, next);        
-        } else {
-            console.log("processing tvpurl")
-            next(null, tvpUrl);
-        }
-    }; 
-    
-    function processFrameContent(iframeUrl, next){
+    ;
+
+    function spawnVLC(videoUrl) {
+        PROCESS.spawn("vlc", [videoUrl], {detached: true});
+
+        eventBus.emit(EventType.EndRequest, "end");
+    }
+    ;
+
+
+    function processFrameContent(iframeUrl) {
         console.log("Looking for objectId in body...");
         console.log("Getting iframeUrl: " + iframeUrl);
-        REQUEST(iframeUrl, function(err, response, body, next){
+        REQUEST(iframeUrl, function(err, response, body, next) {
             $ = CHEERIO.load(body);
             var tvpUrl = $(CONSTANTS.initParams).attr("value");
             var arr = tvpUrl.split(",");
@@ -204,104 +114,60 @@ var videoGrabber = function() {
                 }
             }
             console.log("video_id param: " + objectId);
-            console.log("commented")
-            runVlc(objectId, next);
-        }); 
-    };
-    
-    
-    function test(url){
-        
-        FUTURES.sequence()
-            .then(function(next) {
-                console.log("RUN request");
-                REQUEST(url, next);
-            })
-            .then(function(next, err, response, body){
-                $ = CHEERIO.load(body);
-                var idv = $(CONSTANTS.kabaretMainIDVparam).val();
-                console.log("idv: " + idv);
-                next(err, idv);
-            })
-            .then(function(next, err, idv){
-                var url = CONSTANTS.kabaretLoadSkecz + idv;
-                REQUEST(url,next);
-            })
-            .then(function(next, err, response, body){
-                console.log("Getting: " + url);
-                $ = CHEERIO.load(body);
-                var tvpUrl = $(CONSTANTS.kabaretIframe).attr('src');
-                console.log("tvpUrl: " + tvpUrl);
-                var objectId = getTvpObjectIdFromURL(tvpUrl);
-                console.log("objectId: " + objectId);
-                
-                next(err, objectId, tvpUrl);
-            })
-             .then(function(next, err, objectId, tvpUrl){
-                console.log("Trying to launch player..");
-                if (objectId != 'player') {
-                    FUTURES.sequence()
-                        .then(function(next){
-                            var urlInfo = CONSTANTS.tvpVideoInfo + objectId;
-                            REQUEST(urlInfo, next);
-                        })
-                        .then(function(next, err, response, body){
-                            console.log("Getting: " + url);
-                            var json = JSON.parse(body);
-                            console.log("VideoUrl: " + json.video_url);
-                            spawnVLC(json.video_url);   
-                        });
-                } else {
-                    console.log("processing tvpurl")
-                    next(err, tvpUrl);
-                }
-            })
-            .then(function(next, err, iframeUrl){
-                console.log("Looking for objectId in body...");
-                console.log("Getting iframeUrl: " + iframeUrl);
-                REQUEST(iframeUrl, next); 
-            })
-            .then(function(next, err, response, body){
-                $ = CHEERIO.load(body);
-                var tvpUrl = $(CONSTANTS.initParams).attr("value");
-                var arr = tvpUrl.split(",");
-                var objectId = "xxxxx";
-                for (var i = 0; i < arr.length; i++) {
-                    if (arr[i].indexOf("video_id") == 0) {
-                        objectId = arr[i].split("=")[1];
-                        break;
-                    }
-                }
-                console.log("video_id param: " + objectId);
-                next(err, objectId);
-            })
-            .then(function(next, err, objectId){
-                FUTURES.sequence()
-                    .then(function(next){
-                        var urlInfo = CONSTANTS.tvpVideoInfo + objectId;
-                        REQUEST(urlInfo, next);
-                    })
-                    .then(function(next, err, response, body){
-                        console.log("Getting: " + url);
-                        var json = JSON.parse(body);
-                        console.log("VideoUrl: " + json.video_url);
-                        spawnVLC(json.video_url);   
-                    });
-            });
-            
-    };
-};
 
+            eventBus.emit(EventType.NumberObjectId, objectId);
+            ;
+        });
+    }
+    ;
+    
+    this.process = function(url, response){
+        res = response;
+        eventBus.emit(EventType.BeginProcess, url);
+    }
 
+    eventBus.on(EventType.BeginProcess, function(url) {
+        console.log("====> EVENT: " + EventType.BeginProcess);
+        getIdvParamFormAddress(url);
+    });
+
+    eventBus.on(EventType.IdvEvent, function(idv) {
+        console.log("====> EVENT: " + EventType.IdvEvent);
+        getObjectIdFromPageSkecz(idv);
+    });
+
+    eventBus.on(EventType.NumberObjectId, function(objectId) {
+        console.log("====> EVENT: " + EventType.NumberObjectId);
+        processTvpVideoInfo(objectId);
+    });
+
+    eventBus.on(EventType.SpawnVlc, function(videoUrl) {
+        console.log("====> EVENT: " + EventType.SpawnVlc);
+        spawnVLC(videoUrl);
+    });
+
+    eventBus.on(EventType.PlayerObjectId, function(iframeUrl) {
+        console.log("====> EVENT: " + EventType.PlayerObjectId);
+        processFrameContent(iframeUrl);
+    });
+    
+    eventBus.on(EventType.EndRequest, function() {
+        console.log("====> EVENT: " + EventType.EndRequest);
+        res.writeHead(200, {'Content-Type': 'text/html'});
+
+        res.write("ok: ");
+
+        console.log("DONE");
+        res.end();
+    });
+}
+;
+
+var vg = new VideoGrabber();
 
 HTTP.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-//    res.write("ok: " + getMainPageContents(getAddressParam(req)));
-    var vg = new videoGrabber();
-    var url = vg.getAddressParam(req);
-    console.log("result: " + vg.getMainPageContents(url));
-    res.write("ok: ");
 
+    var url = vg.getAddressParam(req);
+    vg.process(url, res);
     
-    res.end();
 }).listen(9000);
